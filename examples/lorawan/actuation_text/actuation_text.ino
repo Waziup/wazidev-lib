7bbff92d3ccb5c75518534c0a94327dbb8eaaaf1
j-forster <25847959+j-forster@users.noreply.github.com>
2021-01-11 12:50:18 +0100

    Add more examples

--- examples/lorawan/actuation_text/actuation_text.ino
+++ examples/lorawan/actuation_text/actuation_text.ino
@@ -1,5 +1,5 @@
-
 #include <WaziDev.h>
+#include <xlpp.h>
 
 // LoRaWANKey is used as both NwkSKey (Network Session Key) and Appkey (AppKey) for secure LoRaWAN transmission.
 // Copy'n'paste the key to your Wazigate: 23158D3BBC31E6AF670D195B5AED5525
@@ -8,36 +8,27 @@ unsigned char LoRaWANKey[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 
 unsigned char DevAddr[4] = {0x26, 0x01, 0x1D, 0x87};
 // You can change the Key and DevAddr as you want.
 
-
 WaziDev wazidev;
 
 void setup()
 {
-  Serial.begin(38400);
-
-  serialPrintf("LoRaWAN setup ...\n");
-  uint8_t e = wazidev.setupLoRaWAN(DevAddr, LoRaWANKey);
-  if (e != 0)
-  {
-    serialPrintf("Err %d\n", e);
-    while (true);
-    return;
-  }
-
-  serialPrintf("OK\n");
+    Serial.begin(38400);
+    wazidev.setupLoRaWAN(DevAddr, LoRaWANKey);
 }
 
-char payload[255];
+XLPP xlpp(120);
 
 void loop(void)
 {
-  strcpy(payload, "hello :D");
-  uint8_t len = strlen(payload);
-  serialPrintf("Send: [%d] \"%s\"\n", len, payload);
+  // 1
+  // Create xlpp payload.
+  xlpp.reset();
+  xlpp.addTemperature(1, 20.3); // °C
 
-
+  // 2.
+  // Send paload with LoRaWAN.
   serialPrintf("LoRaWAN send ... ");
-  uint8_t e = wazidev.sendLoRaWAN(payload, len);
+  uint8_t e = wazidev.sendLoRaWAN(xlpp.buf, xlpp.len);
   if (e != 0)
   {
     serialPrintf("Err %d\n", e);
@@ -45,12 +36,16 @@ void loop(void)
     return;
   }
   serialPrintf("OK\n");
-
   
+  // 3.
+  // Receive LoRaWAN message (waiting for 6 seconds only).
   serialPrintf("LoRa receive ... ");
   uint8_t offs = 0;
   long startSend = millis();
-  e = wazidev.receiveLoRaWAN(payload, &offs, &len, 6000);
+  uint8_t offset;
+  uint8_t len;
+  uint8_t buf[255];
+  e = wazidev.receiveLoRaWAN(buf, &offset, &len, 6000);
   long endSend = millis();
   if (e != 0)
   {
@@ -65,11 +60,14 @@ void loop(void)
     return;
   }
   serialPrintf("OK\n");
-
+  
   serialPrintf("Time On Air: %d ms\n", endSend-startSend);
-  serialPrintf("Received: [%d] \"%s\"\n", len, payload+offs);
   serialPrintf("LoRa SNR: %d\n", wazidev.loRaSNR);
   serialPrintf("LoRa RSSI: %d\n", wazidev.loRaRSSI);
 
+  // 4.
+  // Read message.
+  serialPrintf("Message: %s (%d Bytes)\n", buf+offset, len);
+  
   delay(60000);
 }
